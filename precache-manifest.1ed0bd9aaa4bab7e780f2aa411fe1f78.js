5614d69d9eb33714d769abf80439ab24ee7bf30c
gaponukz <gaponukz54@gmail.com>
2020-05-12 13:14:13 +0300

    Updates

---
+++ precache-manifest.1ed0bd9aaa4bab7e780f2aa411fe1f78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37dd5a5ddab46d8ede7753ceee6275c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a705258bab64a5423bad",
+    "url": "/static/css/main.60d4db85.chunk.css"
+  },
+  {
+    "revision": "ecb5caca8739742305fb",
+    "url": "/static/js/2.8ba51637.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8ba51637.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a705258bab64a5423bad",
+    "url": "/static/js/main.d805b7cd.chunk.js"
+  },
+  {
+    "revision": "8f042c284597415f8869",
+    "url": "/static/js/runtime-main.c8325658.js"
+  }
+]);
