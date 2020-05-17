f7364ea4517a0a28d3fb891e41945bc4592dc044
gaponukz <gaponukz54@hmail.com>
2020-05-17 13:05:15 +0300

    Updates

--- country.js
+++ country.js
@@ -4,7 +4,7 @@ fetch("https://pomber.github.io/covid19/timeseries.json")
         let active_cases = []
         let st_date = []
         let value = window.location.hash.substring(1).substring(1)
-        data[value ? value : ""].forEach(({ date, confirmed, recovered, deaths }) => {
+        data["US"].forEach(({ date, confirmed, recovered, deaths }) => {
             active_cases.push(confirmed - recovered - deaths)
             st_date.push(date)
         }
