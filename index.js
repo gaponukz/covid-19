067f275d590b4404c9892fa304c14d02e826e181
gaponukz <gaponukz54@hmail.com>
2020-05-19 12:32:17 +0300

    Updates

--- index.js
+++ index.js
@@ -67,45 +67,43 @@ fetch("https://pomber.github.io/covid19/timeseries.json")
         })
     }
     else {
-        window.onload = () => {
-            let active_cases = []
-            let st_date = []
-            let value = window.location.hash.substring(1).substring(1)
-            data[value ? value.replace('%20', ' ').replace('%20', ' ') : ""]
-            .forEach(({ date, confirmed, recovered, deaths }) => {
-                active_cases.push(confirmed - recovered - deaths)
-                st_date.push(date)
-            }
-        )
-            const myChart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: st_date,
-                    datasets: [{
-                        label: '# of Patients',
-                        data: active_cases,
-                        lineTension: 0,
-                        backgroundColor: 'transparent',
-                        borderColor: '#007bff',
-                        borderWidth: 4,
-                        radius: 0,
-                        pointBackgroundColor: '#007bff'
-                    }]
-                },
-                options: {
-                    scales: {
-                    yAxes: [{
-                        ticks: {
-                        beginAtZero: false
-                        }
-                    }]
-                    },
-                    legend: {
-                    display: false
-                    }
-                }
-            })
+        let active_cases = []
+        let st_date = []
+        let value = window.location.hash.substring(1).substring(1)
+        data[value ? value.replace('%20', ' ').replace('%20', ' ') : ""]
+        .forEach(({ date, confirmed, recovered, deaths }) => {
+            active_cases.push(confirmed - recovered - deaths)
+            st_date.push(date)
         }
+    )
+        const myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: st_date,
+                datasets: [{
+                    label: '# of Patients',
+                    data: active_cases,
+                    lineTension: 0,
+                    backgroundColor: 'transparent',
+                    borderColor: '#007bff',
+                    borderWidth: 4,
+                    radius: 0,
+                    pointBackgroundColor: '#007bff'
+                }]
+            },
+            options: {
+                scales: {
+                yAxes: [{
+                    ticks: {
+                    beginAtZero: false
+                    }
+                }]
+                },
+                legend: {
+                display: false
+                }
+            }
+        })
     }
 })
 
